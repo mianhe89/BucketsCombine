5a351a1e48d29b7ecb001eeece86ba7f1da98a93
mianhe89 <mianhe89@gmail.com>
2022-07-03 20:45:00 +0900

    Server0703_01

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const https = require("https");
+const fs = require("fs");
+const app = express();
+
+const controllers = require("./controllers");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["*"],
+    credentials: true,
+    methods: ["GET", "OPTIONS"],
+  })
+);
+app.use(cookieParser());
+
+app.get("/", function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send("Hello World!");
+});
+// "hello World" 를 데이터베이스에서 찾아 변수에 저장한 뒤 사용 가능
+app.listen(port, () => {
+  console.log(`서버가 ${port}번에서 작동중입니다.`);
+});
