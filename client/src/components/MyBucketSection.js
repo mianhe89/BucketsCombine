080ba644500bbc7c8fed25526ca6ff683c05b004
SHREWDIE-38 <tyoon225@gmail.com>
2022-07-08 12:36:28 +0900

    build mypage

--- client/src/components/MyBucketSection.js
+++ client/src/components/MyBucketSection.js
@@ -2,19 +2,14 @@ import React from 'react';
 
 export default function MyBucketSection(){
   return(
-    <div className='main-section'>
-      <div className="main-ment">
-      </div>
-      <div className='videos'>
-        <video className="video" autoPlay muted loop>
-          <source src="/videos/A.mp4" type="video/mp4"></source>
-        </video>
-        <video className="video" autoPlay muted loop>
-          <source src="/videos/B.mp4" type="video/mp4"></source>
-        </video>
-        <video className="video" autoPlay muted loop>
-          <source src="/videos/C.mp4" type="video/mp4"></source>
-        </video>
+    <div id='mybucket-section'>
+      <div className='card-list-column'>
+        <button className='create-card-button'>+</button>
+        <div className='card-column'>
+          <div className='card-progress'/>
+          <div className='card-column-info'>
+          </div>
+        </div>
       </div>
     </div>
   )
