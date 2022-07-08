cbb7ca6cc9661ce3aed61c61f83b50921013dd8c
leehunds <leehundsn@naver.com>
2022-07-08 20:59:43 +0900

    add myProFile

--- client/src/components/MyProfileSection.js
+++ client/src/components/MyProfileSection.js
@@ -3,6 +3,15 @@ import React from 'react';
 export default function MyProfileSection(){
   return(
     <div id='myprofile-section'>
+      <div className="box-myprofile">
+        <div className="box-photo" ></div>
+        <div className="profile-info-section">
+        <div className="profile-info-nickname"></div>
+        <div className="profile-info-age"></div>
+        <div className="profile-info-gender"></div>
+        </div>
+
+      </div>
     </div>
   )
 }
