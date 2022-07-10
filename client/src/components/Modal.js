fe5873cbd98f80000f463501cc32ff4b97fce4a7
leeyunchang <hujy4023@gamil.com>
2022-07-10 15:45:32 +0900

    mainpage modal css update

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -8,6 +8,8 @@ const Modal = () => {
         <aisde className="modal-container">
             <div className="modal">
                 <h4 className=" modal-title">카드 제목</h4>
+                    <div className="card-tag">태그</div>
+                    <button type="button" className="userInfo-btn">참석한 유저 이름</button>
                     <button type="button" className="btn-confirm-btn"
                     onClick={()=> {
                         dispatch(closeModal());
@@ -22,6 +24,7 @@ const Modal = () => {
                     </button>
                     <div className="card-img">사진</div>
                     <div className="card-info">설명</div>
+                    <div className="card-info-text">버킷 내용</div>
                 </div>
         </aisde>
     );
