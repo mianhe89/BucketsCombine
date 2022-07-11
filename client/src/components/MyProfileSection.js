afb9ee254c89579911bf6a77214939dd0b9002c6
leehunds <leehundsn@naver.com>
2022-07-11 13:06:07 +0900

    new Profile

--- client/src/components/MyProfileSection.js
+++ client/src/components/MyProfileSection.js
@@ -4,14 +4,25 @@ export default function MyProfileSection(){
   return(
     <div id='myprofile-section'>
       <div className="box-myprofile">
-        <div className="box-photo" ></div>
+        <div>
+        <div className="box-photo">사진</div>
         <div className="profile-info-section">
-        <div className="profile-info-nickname"></div>
-        <div className="profile-info-age"></div>
-        <div className="profile-info-gender"></div>
+          <div className="profile-info-detail">
+            <div className="profile-info-email">사용자@이메일</div>
+            <div className="profile-info-nickname">닉네임</div>
+            <div className="profile-info-age"></div>
+          <div className="profile-info-gender"></div>
+          </div>
         </div>
-
-      </div>
+        </div>
+        <div className="box-profile-introducing"><text>기존소개글이 적혀있고 수정가능</text></div>
+        
+        <div className="profile-change-button">
+          <button className="change-password-button">비밀번호 변경</button>
+          <button className="change-button">변경</button>
+        </div>
+    </div>
+    <div className="profile-signout"> 회원탈퇴 </div>
     </div>
   )
 }
