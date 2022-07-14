58274a558209f6d874304aadfe367317689c4a9d
SHREWDIE-38 <tyoon225@gmail.com>
2022-07-14 15:47:58 +0900

    repair and update myprofile

--- client/src/components/MyProfileSection.js
+++ client/src/components/MyProfileSection.js
@@ -6,248 +6,189 @@ const MyProfileWrap = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: flex-start;
+    align-items: center;
     width: calc(100vw - 120px);
     height: 100vh;
     margin-left: 120px;
   }
-  .box-myprofile {
+  .myprofile-container {
     display: flex;
-    margin: 0 auto;
     background-color: #ededed;
-    width: 85vh;
-    height: 70vh;
     border-radius: 20px;
     flex-direction: column;
     height: 588px;
     width: 847px;
-    left: 372px;
-    top: 177px;
     border-radius: 20px;
-
-    
+    border: 10px dotted white;
   }
   .box-photo {
-    margin-top: 23px;
-    margin-left: 23px;
     background-color: white;
     width: 148px;
     height: 148px;
-    float: left;
     box-shadow: 0px 4px 4px 0px #00000040;
-    height: 148px;
-    width: 148px;
-    left: 395px;
-    top: 200px;
     border-radius: 15px;
     border: 1px solid #969696;
     font-family: Inter;
     font-size: 18px;
-    font-weight: 700;
-    line-height: 22px;
-    letter-spacing: 0em;
     text-align: center;
-    color: #969696;
-    
+    line-height: 145px;
+    color: #969696;  
+  }
+
+  .partition {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 28px;
   }
   .profile-info-section {
     display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     width: 350px;
-    height: 30vh;
-    float: right;
-    margin-right: 30px;
-    margin-top:28
-    
+    height: 200px;
   }
-  .profile-info-detail {
-    margin-top: 20px;
-    margin-left: 30px
-  }
-  .profile-info-email{
+
+  .profile-info-email {
     height: 40px;
-    width: 330px;
-    left: 861px;
-    top: 205px;
     border-radius: 5px;
-    background: #8F8F8F;
+    background: #9F9F9F;
     border: none;
     font-family: 'Inter';
     font-style: normal;
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 19px;
-
-    color: #FFFFFF;
+    font-size: 14px;
+    color: white;
+    line-height: 40px;
+    padding: 0px 0px 0px 8px;
   }
   .profile-info-nickname {
     height: 40px;
-    width: 330px;
-    left: 861px;
-    top: 257px;
     border-radius: 5px;
-    margin-top: 12px;
     background-color: white;
     border: none;
+    box-shadow: 0px 4px 4px 0px #00000040;
+    border: 1px solid #969696;
+    box-sizing: border-box;
+    padding: 8px;
   }
   .profile-info-age {
     height: 40px;
-    width: 330px;
-    left: 861px;
-    top: 257px;
     border-radius: 5px;
-    margin-top: 12px;
     background-color: white;
     border: none;
+    box-shadow: 0px 4px 4px 0px #00000040;
+    border: 1px solid #969696;
+    box-sizing: border-box;
+    padding: 5px;
   }
   .profile-info-gender {
     height: 40px;
-    width: 330px;
-    left: 861px;
-    top: 257px;
     border-radius: 5px;
-    margin-top: 12px;
     background-color: white;
     border: none;
+    box-shadow: 0px 4px 4px 0px #00000040;
+    border: 1px solid #969696;
+    box-sizing: border-box;
+    padding: 5px;
   }
-  .box-profile-introducing 
-  {
+
+  .profile-introducing {
+    height: 100%;
     border-radius: 10px;
-    margin-left: 23px;
-    margin-right: 27px;
-    margin-top: 25px;
-    margin-bottom: 20px;
+    margin: 28px 28px 0px 28px;
+    padding: 20px;
     background-color: white;
     box-shadow: 0px 4px 4px 0px #00000040;
     border: 1px solid #969696;
     box-sizing: border-box;
-    width: 797px;
-    height: 251px;
-    left: 395px;
-    top: 432px;
-
   }
-  .profile-change-button {
+
+  .change-buttons {
     display: flex;
     justify-content: end;
-    width: 847px;
-    height: 82px;
-
+    margin: 10px 32px 10px 32px;
   }
+
   .change-password-button {
-    display: flex;
-    margin-right: 15px;
     border-radius: 10px;
     border: none;
-    background-color: black;
+    background: #323232;
     color: white;
     height: 31px;
     width: 133px;
-    left: 969px;
-    top: 712px;
     border-radius: 10px;
-
-  }
-  .change-password-btw-detail {
-    width: 116px;
-    height: 19px;
-    left: 986px;
-    top: 717px;
-
     font-family: 'Inter';
     font-style: normal;
-    font-weight: 700;
     font-size: 16px;
-    line-height: 19px;
-    margin-top: 5px;
-
+    font-weight: 700;
+    margin: 5px;
   }
-  .change-button {
-    display: flex;
-    justify-content: flex-end;
+
+  .change-profile-button {
+    height: 31px;
+    width: 75px;
     margin-right: 27px;
     border:none;
     color: white;
-    height: 31px;
-    width: 75px;
     left: 1117px;
     top: 712px;
     border-radius: 10px;
     background: #323232;
-
-    .change-button-detail {
-    width: 58px;
-    height: 19px;
-    left: 1139px;
-    top: 717px;
-
     font-family: 'Inter';
     font-style: normal;
     font-weight: 700;
     font-size: 16px;
-    line-height: 19px;
-    margin-top: 5px;
-    }
-
-
-
+    margin: 5px;
   }
-  .profile-signout {
-    width: 83px;
-    height: 22px;
-    left: 379px;
-    top: 782px;
-
+  .withdrawal-button {
     font-family: 'Inter';
     font-style: normal;
     font-weight: 700;
-    font-size: 18px;
-    line-height: 22px;
-    /* identical to box height */
+    font-size: 15px;
     color: #969696;
-    margin-left: 250px;
-    margin-top: 17px;
+    background-color: transparent;
+    border: none;
+    margin-top: 20px;
+    margin-right : 750px;
   }
 `
 
-export default function MyProfileSection(){
-  return(
+export default function MyProfileSection() {
+  return (
     <MyProfileWrap>
-    <div id='myprofile-section'>
-      <div className="box-myprofile">
-        <div>
-        <div className="box-photo">사진</div>
-        <div className="profile-info-section">
-          <div className="profile-info-detail">
-            <input className="profile-info-email" type="text" placeholder="사용자@email"></input>
-            <input className="profile-info-nickname" type="text" placeholder="닉네임"></input>
-          <select className="profile-info-age" required>
-			      <option value="" selected>연령대</option>
-			      <option value="teenages">10대</option>
-			      <option value="twenty">20대</option>
-			      <option value="thirty">30대</option>
-            <option value="forty">40대</option>
-            <option value="fifty">50대</option>
-            <option value="sixty">60대</option>
-            <option value="seventy">70대</option>
-		      </select>
-          <select className="profile-info-gender" required>
-			      <option value="" selected>성별</option>
-			      <option value="teenages">남자</option>
-			      <option value="twenty">여자</option>
-			      <option value="thirty">선택안함</option>
-		      </select>
+      <div id='myprofile-section'>
+        <div className="myprofile-container">
+          <div className='partition'>
+            <div className="box-photo">사진</div>
+            <div className="profile-info-section">
+                <div className="profile-info-email">사용자@email</div>
+                <input className="profile-info-nickname" type="text" placeholder="닉네임"/>
+                <select className="profile-info-age">
+                  <option value="age" selected>연령대</option>
+                  <option value="teenages">10대</option>
+                  <option value="twenty">20대</option>
+                  <option value="thirty">30대</option>
+                  <option value="forty">40대</option>
+                  <option value="fifty">50대</option>
+                  <option value="sixty">60대</option>
+                  <option value="seventy">70대</option>
+                </select>
+                <select className="profile-info-gender">
+                  <option value="" selected>성별</option>
+                  <option value="teenages">남자</option>
+                  <option value="twenty">여자</option>
+                  <option value="thirty">선택안함</option>
+                </select>
+            </div>
+          </div>
+          <textarea className="profile-introducing" placeholder="소개글을 작성해주세요"/>
+          <div className="change-buttons">
+            <button className="change-password-button">비밀번호 변경</button>
+            <button className="change-profile-button">변경</button>
           </div>
         </div>
-        </div>
-        <div><input className="box-profile-introducing" type="text" placeholder="기존 소개글이 적혀있고 수정가능"></input></div>
-        
-        <div className="profile-change-button">
-          <button className="change-password-button"><div className="change-password-btw-detail">비밀번호 변경 </div></button>
-          <button className="change-button"><div className="change-button-detail">변경</div></button>
-        </div>
-    </div>
-    <div className="profile-signout"> 회원탈퇴 </div>
-    </div>
+        <button className="withdrawal-button">회원탈퇴</button>
+      </div>
     </MyProfileWrap>
   )
 }
