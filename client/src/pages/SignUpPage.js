8fc707433ca2b7d9ce7cd78852d876fc5a7053de
beggin0 <beggin823@gmail.com>
2022-07-17 10:59:45 +0900

    client signup modify

--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import axios from 'axios';
-import styled from 'styled-components'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import styled from "styled-components";
 
 const SignUpPageWrap = styled.div`
   .body {
@@ -14,7 +14,7 @@ const SignUpPageWrap = styled.div`
     right: 15px;
     width: 100px;
     height: 30px;
-    background: #FFC700;
+    background: #ffc700;
     border-radius: 15px;
     border: 0;
   }
@@ -46,14 +46,13 @@ const SignUpPageWrap = styled.div`
     margin: 20px;
     border-radius: 5px;
   }
-  .login_signupbox{
+  .login_signupbox {
     display: flex;
     flex-direction: column;
     padding: 5px 40px;
     margin: 2px 100px;
     width: 250px;
     border-radius: 5px;
-    
   }
   .login_box {
     background-color: orange;
@@ -71,8 +70,6 @@ const SignUpPageWrap = styled.div`
     margin: 7px;
     height: 30px;
     border-radius: 5px;
-    
-    
   }
   .login_signup {
     flex: flex;
@@ -85,20 +82,20 @@ const SignUpPageWrap = styled.div`
     border-radius: 5px;
   }
   #login_email {
-	width: 246px;
-	height: 32px;
+    width: 246px;
+    height: 32px;
     padding: 2px 40px;
     margin: 10px;
     border-radius: 5px;
-	border: none;
+    border: none;
   }
   #login_password {
-	width: 246px;
-	height: 32px;
+    width: 246px;
+    height: 32px;
     padding: 2px 40px;
     margin: 10px;
     border-radius: 5px;
-	border: none;
+    border: none;
   }
   .signup_container {
     display: flex;
@@ -107,33 +104,33 @@ const SignUpPageWrap = styled.div`
     min-height: 100vh;
   }
   #signup_repassword {
-	width: 246px;
-	height: 32px;
+    width: 246px;
+    height: 32px;
     padding: 2px 40px;
     margin: 10px;
     border-radius: 5px;
-	border: none;
+    border: none;
   }
   #login_nickname {
-	width: 246px;
-	height: 32px;
+    width: 246px;
+    height: 32px;
     padding: 2px 40px;
     margin: 10px;
     border-radius: 5px;
-	border: none;
+    border: none;
   }
   .btn_old {
     padding: 5px 40px;
     margin: 10px;
     width: 330px;
-	height: 40px;
+    height: 40px;
     border-radius: 5px;
   }
   .btn_gender {
     padding: 5px 40px;
     margin: 10px;
     width: 330px;
-	height: 40px;
+    height: 40px;
     border-radius: 5px;
   }
 
@@ -144,17 +141,17 @@ const SignUpPageWrap = styled.div`
     justify-content: center;
     margin: 4px;
     width: 330px;
-    background: #FFC700;
+    background: #ffc700;
     border-radius: 5px;
   }
-`
+`;
 export default function SignUpPage() {
   const [userinfo, setuserinfo] = useState({
-    login_email: '',
-    login_password: '',
-    login_username: '',
+    email: "",
+    password: "",
+    username: "",
   });
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
   const history = useHistory();
   const handleInputValue = (key) => (e) => {
     setuserinfo({ ...userinfo, [key]: e.target.value });
@@ -166,55 +163,83 @@ export default function SignUpPage() {
     //        history.push("/");
     //
     // TODO : 모든 항목을 입력하지 않았을 경우 에러를 표시해야 합니다.
-    if(userinfo.longin_email === '' || userinfo.login_password === '' || userinfo.login_nickname === '' || userinfo.login_repassword){
-      setErrorMessage('모든 항목은 필수입니다')
+    if (
+      userinfo.email === "" ||
+      userinfo.password === "" ||
+      userinfo.username === ""
+    ) {
+      setErrorMessage("모든 항목은 필수입니다");
     } else {
-      axios.post('ec2-52-79-247-243.ap-northeast-2.compute.amazonaws.com', {
-        login_email: userinfo.login_email,
-        login_password: userinfo.login_password,
-        signup_repassword: userinfo.signup_repassword,
-        login_nickname: userinfo.login_nickname
-      })
-      .then(res => {
-        history.push("/")
-      })
+      axios
+        .post("ec2-52-79-247-243.ap-northeast-2.compute.amazonaws.com", {
+          email: userinfo.email,
+          password: userinfo.password,
+          username: userinfo.username,
+        })
+        .then((res) => {
+          history.push("/");
+        });
     }
   };
-	return (
-		<SignUpPageWrap>
-		<div className="body">
-<div className="signin_section">
-<button id="login_cancle">취소</button>
-	<div className="signin_container">
-	<img src="images/bucketscombine_logo.png" alt="no" width="120px" height="120px"></img>
-		<div className="login_title">BucketsCombine</div>
-	   	<input id="login_email" type="email" placeholder="이메일" onChange={handleInputValue("login_email")} />
-		  <input id="login_password" type="password" placeholder="비밀번호" onChange={handleInputValue("login_password")} />
-		  <input id="signup_repassword" type="password" placeholder="비밀번호 재확인" onChange={handleInputValue("login_password")}/>
-		  <input id="login_nickname" type="text" placeholder="닉네임" />
-		<select className="btn_old" required>
-			<option value="">연령대</option>
-			<option value="teenages">10대</option>
-			<option value="twenty">20대</option>
-			<option value="thirty">30대</option>
-			<option value="forty">40대</option>
-			<option value="fifty">50대</option>
-			<option value="sixty">60대</option>
-			<option value="seventy">70대</option>
-		</select>
-		<select className="btn_gender" required>
-			<option value="" >성별</option>
-			<option value="teenages">남자</option>
-			<option value="twenty">여자</option>
-			<option value="thirty">선택안함</option>
-		</select>
-		<button className="signup_signup"
-            type='submit'
-            onClick={handleSignup}>가입하기</button>
-          <div className='alert-box'>{errorMessage}</div>
-	</div>
-</div>
-</div>
-</SignUpPageWrap>
-)
+  return (
+    <SignUpPageWrap>
+      <div className="body">
+        <div className="signin_section">
+          <button id="login_cancle">취소</button>
+          <div className="signin_container">
+            <img
+              src="images/bucketscombine_logo.png"
+              alt="no"
+              width="120px"
+              height="120px"
+            ></img>
+            <div className="login_title">BucketsCombine</div>
+            <input
+              id="login_email"
+              type="email"
+              placeholder="이메일"
+              onChange={handleInputValue("login_email")}
+            />
+            <input
+              id="login_password"
+              type="password"
+              placeholder="비밀번호"
+              onChange={handleInputValue("login_password")}
+            />
+            <input
+              id="signup_repassword"
+              type="password"
+              placeholder="비밀번호 재확인"
+              onChange={handleInputValue("login_password")}
+            />
+            <input id="login_nickname" type="text" placeholder="닉네임" />
+            <select className="btn_old" required>
+              <option value="">연령대</option>
+              <option value="teenages">10대</option>
+              <option value="twenty">20대</option>
+              <option value="thirty">30대</option>
+              <option value="forty">40대</option>
+              <option value="fifty">50대</option>
+              <option value="sixty">60대</option>
+              <option value="seventy">70대</option>
+            </select>
+            <select className="btn_gender" required>
+              <option value="">성별</option>
+              <option value="teenages">남자</option>
+              <option value="twenty">여자</option>
+              <option value="thirty">선택안함</option>
+            </select>
+            <button
+              className="signup_signup"
+              type="submit"
+              onClick={handleSignup}
+            >
+              가입하기
+            </button>
+            <div className="alert-box">{errorMessage}</div>
+          </div>
+        </div>
+      </div>
+    </SignUpPageWrap>
+  );
 }
